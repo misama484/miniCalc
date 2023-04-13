489ff8dc7752a13ffc499e0f7e56d581c7882b8a
Miguel Sanchez Martínez <misama484@gmail.com>
2023-04-13 10:44:35 +0200

    Funcionalidad finalizada y aplicado algo de Bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'miniCalc';
+  title = 'MiniCalculadora Angular';
+
+  numero1:number = 0;
+  numero2:number = 0;
+  resultado:number = 0;
+
+  Sumar():void{
+    this.resultado = this.numero1 + this.numero2;
+  }
+  Restar():void{
+    this.resultado = this.numero1 - this.numero2;
+  }
+  Multiplicar():void{
+    this.resultado = this.numero1 * this.numero2;
+  }
+  Dividir():void{
+    this.resultado = this.numero1 / this.numero2;
+  }
+
 }
